90d51c677231424ef0ad48229097f7ebedfa41cc
Dhruv Sharma <dhruvs8167@gmail.com>
2019-09-11 16:34:58 +0530

    Initial commit

---
+++ Assets/Script/Bombspawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bombspawner : MonoBehaviour {
+    public GameObject bomb;
+    float randX;
+    Vector2 whereToSpwan;
+    public float spwanRate = 10f;
+    float nextspwan = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextspwan)
+        {
+            nextspwan = Time.time + spwanRate;
+            randX = Random.Range (-2.5f , 2.5f);
+            whereToSpwan = new Vector2(randX, transform.position.y);
+            Instantiate(bomb, whereToSpwan, Quaternion.identity);
+        }
+	}
+}
---
+++ Assets/Script/ScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+    public Text timertext;
+    private float starttime;
+	// Use this for initialization
+	void Start () {
+        starttime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = Time.time - starttime;
+        string min = ((int)t / 60).ToString();
+        string sec = (t % 60).ToString();
+        timertext.text = min + ":" + sec;
+    }
+}
